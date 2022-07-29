104a388a7e04e4f9bbcf079646392724e07268aa
tukasa <497116162@qq.com>
2022-07-29 23:55:36 +0800

    封装按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <button @click="colorChange">换肤</button>
+    <Vbutton :title="'按钮组件'" :type="'large'">
+      <template>
+        <img src="https://img01.yzcdn.cn/vant/cat.jpeg" alt="">
+      </template>
+    </Vbutton>
     <div class="todoapp">
       <todo-header></todo-header>
       <todo-main></todo-main>
@@ -13,6 +18,7 @@
 import TodoFooter from './components/TodoFooter.vue' // shift + alt + 下
 import TodoHeader from './components/TodoHeader.vue' // shift + alt + 下
 import TodoMain from './components/TodoMain.vue' // shift + alt + 下
+import Vbutton from "./components/Vbutton.vue";
 export default {
   data() {
     return {}
@@ -22,6 +28,7 @@ export default {
     TodoFooter,
     TodoHeader,
     TodoMain,
+    Vbutton
   },
 
   created() {},
---
+++ src/components/Vbutton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button :class="{'type1': type === 'large'}">
+    <slot>
+      <span>{{ title }}</span>
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+
+  created() {
+    console.log(this.title);
+  },
+
+  methods: {},
+};
+</script>
+
+<style  scoped>
+.type1 {
+  width: 200px;
+  height: 100px;
+  font-size:30px;
+  background: pink;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
